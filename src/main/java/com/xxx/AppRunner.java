d1cf94b5ce0c5e167ae7265d2c0e09130adb6248
sergmain <serg_main@yahoo.com>
2018-08-12 01:38:18 +0300

    .

--- src/main/java/com/xxx/AppRunner.java
+++ src/main/java/com/xxx/AppRunner.java
@@ -3,9 +3,16 @@ package com.xxx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+import java.util.Properties;
+
 @SpringBootApplication
 public class AppRunner {
     public static void main(String[] args) {
+        Properties properties = System.getProperties();
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            System.out.println(String.format("%s : %s", entry.getKey(), entry.getValue()));
+        }
         SpringApplication.run(AppRunner.class, args);
     }
 }
